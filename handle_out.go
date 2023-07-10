c9790391e10dc903a945c7576d00d48ecea97708
Salim Nassim <salim.nassim@gmail.com>
2023-07-10 12:13:12 -0400

    debug before send

--- handle_out.go
+++ handle_out.go
@@ -4,12 +4,12 @@ import "github.com/rs/zerolog/log"
 
 func HandleConnectionOut(client *Client, server *Server) {
 	for message := range client.send {
+		log.Debug().Msgf("%s: %s", client.Prefix(), message)
+
 		_, err := client.Write(message + "\r\n")
 		if err != nil {
 			log.Error().Err(err).Msgf("unable to write message to client (%s)", client.connection.RemoteAddr())
 			break
 		}
-
-		log.Debug().Msgf("%s: %s", client.Prefix(), message)
 	}
 }
