82d894c0881c847b2b3a25c3173d6d3f1d53e323
Salim Nassim <salim.nassim@gmail.com>
2023-07-10 12:13:25 -0400

    remove dead code after channel store

--- server.go
+++ server.go
@@ -1,7 +1,6 @@
 package ircd
 
 import (
-	"errors"
 	"net/http"
 	"regexp"
 	"sync"
@@ -22,7 +21,7 @@ type Server struct {
 	mu       *sync.RWMutex
 	name     string
 	clients  ClientStoreable
-	channels map[*Channel]interface{}
+	channels ChannelStoreable
 
 	// regex cache
 	regex map[string]*regexp.Regexp
@@ -41,7 +40,7 @@ func NewServer(config ServerConfig) *Server {
 		mu:       &sync.RWMutex{},
 		name:     config.Name,
 		clients:  NewClientStore(),
-		channels: make(map[*Channel]interface{}),
+		channels: NewChannelStore("default"),
 		regex:    make(map[string]*regexp.Regexp),
 		gauges:   make(map[string]prometheus.Gauge),
 		counters: make(map[string]prometheus.Counter),
@@ -55,7 +54,7 @@ func NewServer(config ServerConfig) *Server {
 
 // Compiles expressions and caches them to a map
 func compileRegexp(server *Server) {
-	rgxNick, err := regexp.Compile(`([a-zA-Z0-9\[\]\|]{2,16})`)
+	rgxNick, err := regexp.Compile(`([a-zA-Z0-9\[\]\|]{2,9})`)
 	if err != nil {
 		log.Panic().Err(err).Msg("unable to compile nickname validation regex")
 	}
@@ -99,7 +98,7 @@ func registerMetrics(server *Server) {
 
 // Returns the number of connected clients, and channels
 func (server *Server) Stats() (int, int) {
-	return server.clients.Size(), len(server.channels)
+	return server.clients.Size(), server.channels.Size()
 }
 
 // Removes client from channels and client map
@@ -107,70 +106,13 @@ func (server *Server) RemoveClient(client *Client) error {
 	server.mu.Lock()
 	defer server.mu.Unlock()
 
-	for channel := range server.channels {
-		for v, c := range channel.clients {
-			if v == client.id {
-				err := channel.RemoveClient(c)
-				if err != nil {
-					return err
-				}
-			}
-		}
+	memberOf := server.channels.MemberOf(client)
+	for _, ch := range memberOf {
+		ch.RemoveClient(client)
 	}
 
 	server.clients.Remove(client)
 	server.gauges["ircd_clients"].Dec()
 
 	return nil
-}
-
-func (server *Server) Channels() []Channel {
-	server.mu.RLock()
-	defer server.mu.RUnlock()
-
-	var channels []Channel
-	for c := range server.channels {
-		channels = append(channels, *c)
-	}
-
-	return channels
-}
-
-// Returns a pointer to channel by name. bool will be true if channel exists
-func (server *Server) Channel(name string) (*Channel, bool) {
-	server.mu.RLock()
-	defer server.mu.RUnlock()
-
-	for channel := range server.channels {
-		if channel.name == name {
-			return channel, true
-		}
-	}
-	return nil, false
-}
-
-// Creates a channel and returns a pointer to it
-func (server *Server) CreateChannel(name string, owner *Client) *Channel {
-	server.mu.Lock()
-	defer server.mu.Unlock()
-
-	channel := NewChannel(name, owner)
-	server.channels[channel] = true
-	server.gauges["ircd_channels"].Inc()
-
-	return channel
-}
-
-// Removes client from channel map
-func (server *Server) RemoveChannel(channel *Channel) error {
-	server.mu.Lock()
-	defer server.mu.Unlock()
-
-	if _, ok := server.channels[channel]; ok {
-		delete(server.channels, channel)
-		server.gauges["ircd_channels"].Dec()
-		return nil
-	}
-
-	return errors.New("channel not found")
 }
