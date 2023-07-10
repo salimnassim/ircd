833249c9609b1a4fe45ae6978a68d79b93759a54
Salim Nassim <salim.nassim@gmail.com>
2023-07-10 01:22:43 -0400

    swap key and value

--- channel_store.go
+++ channel_store.go
@@ -3,32 +3,38 @@ package ircd
 import "sync"
 
 type ChannelStoreable interface {
+	Add(id string, channel *Channel)
+	GetByName(string) (*Channel, bool)
 }
 
 type ChannelStore struct {
 	mu       *sync.RWMutex
 	id       string
-	channels map[*Channel]bool
+	channels map[string]*Channel
 }
 
 func NewChannelStore(id string) *ChannelStore {
 	return &ChannelStore{
 		id:       id,
 		mu:       &sync.RWMutex{},
-		channels: map[*Channel]bool{},
+		channels: make(map[string]*Channel),
 	}
 }
 
 // Adds channel to store
-func (cs *ChannelStore) Add(channel *Channel) {
+func (cs *ChannelStore) Add(id string, channel *Channel) {
 	cs.mu.Lock()
 	defer cs.mu.Unlock()
 
-	cs.channels[channel] = true
+	cs.channels[id] = channel
 }
 
 // Returns a pointer to the channel by name.
 // Boolean returns true if thee channel exists
-// func (cs *ChannelStore) GetByName(channelName string) (*Channel, bool) {
-// 	return
-// }
+func (cs *ChannelStore) GetByName(channelName string) (*Channel, bool) {
+	channel, exists := cs.channels[channelName]
+	if !exists {
+		return nil, false
+	}
+	return channel, true
+}
