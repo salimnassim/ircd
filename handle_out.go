368f78782f025b4f568829cbf105cf28432dd6e0
Salim Nassim <salim.nassim@gmail.com>
2023-07-20 23:43:44 -0400

    send from client

--- handle_out.go
+++ handle_out.go
@@ -1,8 +1,6 @@
 package ircd
 
 import (
-	"bufio"
-
 	"github.com/rs/zerolog/log"
 )
 
@@ -11,18 +9,11 @@ func HandleConnectionOut(client *Client, server *Server) {
 		client.stop <- true
 	}()
 
-	writer := bufio.NewWriter(client.writer)
-
 	for message := range client.send {
 		log.Debug().Msgf("%s: %s", client.Prefix(), message)
-		_, err := writer.WriteString(message + "\r\n")
+		_, err := client.Write(message + "\r\n")
 		if err != nil {
 			log.Error().Err(err).Msgf("unable to write message to client (%s)", client.ip)
-			break
-		}
-		err = writer.Flush()
-		if err != nil {
-			log.Error().Err(err).Msg("unable to flush writer")
 			break
 		}
 	}
