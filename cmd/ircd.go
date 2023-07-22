35217a8151bfdfd204c1290992f4ece32ca353c4
Salim Nassim <salim.nassim@gmail.com>
2023-07-21 21:58:03 -0400

    remove pyroscope

--- cmd/ircd.go
+++ cmd/ircd.go
@@ -5,10 +5,8 @@ import (
 	"net/http"
 	_ "net/http/pprof"
 	"os"
-	"runtime"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/pyroscope-io/client/pyroscope"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -16,34 +14,6 @@ import (
 )
 
 func main() {
-
-	if os.Getenv("PYROSCOPE_ENABLE") != "" {
-		log.Info().Msg("pyroscope is enabled")
-
-		runtime.SetMutexProfileFraction(5)
-		runtime.SetBlockProfileRate(5)
-
-		pyroscope.Start(pyroscope.Config{
-			ApplicationName: "ircd",
-			ServerAddress:   os.Getenv("PYROSCOPE_ADDRESS"),
-			Logger:          nil,
-			Tags:            map[string]string{"hostname": os.Getenv("HOSTNAME")},
-
-			ProfileTypes: []pyroscope.ProfileType{
-				pyroscope.ProfileCPU,
-				pyroscope.ProfileAllocObjects,
-				pyroscope.ProfileAllocSpace,
-				pyroscope.ProfileInuseObjects,
-				pyroscope.ProfileInuseSpace,
-				pyroscope.ProfileGoroutines,
-				pyroscope.ProfileMutexCount,
-				pyroscope.ProfileMutexDuration,
-				pyroscope.ProfileBlockCount,
-				pyroscope.ProfileBlockDuration,
-			},
-		})
-	}
-
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
 	config := ircd.ServerConfig{
@@ -62,8 +32,8 @@ func main() {
 
 	go func() {
 		log.Info().Msg("starting http, listening on :2112")
-		if os.Getenv("PROMETHEUS_ENABLE") != "" {
-			log.Info().Msg("prometheus is enabled")
+		_, ok := os.LookupEnv("PROMETHEUS_ENABLE")
+		if ok {
 			http.Handle("/metrics", promhttp.Handler())
 		}
 		http.HandleFunc("/", server.IndexHandler)
--- go.mod
+++ go.mod
@@ -9,18 +9,16 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
-	github.com/prometheus/client_model v0.3.0 // indirect
-	github.com/prometheus/common v0.42.0 // indirect
-	github.com/prometheus/procfs v0.10.1 // indirect
-	github.com/pyroscope-io/godeltaprof v0.1.0 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
+	github.com/prometheus/client_model v0.4.0 // indirect
+	github.com/prometheus/common v0.44.0 // indirect
+	github.com/prometheus/procfs v0.11.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
 
 require (
 	github.com/google/uuid v1.3.0
-	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/prometheus/client_golang v1.16.0
-	github.com/pyroscope-io/client v0.7.1
-	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
 )
