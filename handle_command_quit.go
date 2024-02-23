cd779ffded90948b1465843162d6c982ad1566f9
Salim Nassim <salim.nassim@gmail.com>
2024-02-22 19:13:30 -0500

    prepend quit:

--- handle_command_quit.go
+++ handle_command_quit.go
@@ -6,14 +6,14 @@ import (
 )
 
 func handleQuit(s *server, c *client, m message) {
-	reason := "leaving"
+	reason := "no reason given"
 	if len(m.params) >= 1 {
 		reason = strings.Join(m.params[0:len(m.params)], " ")
 	}
 
 	for _, ch := range s.channels.memberOf(c) {
 		ch.broadcast(
-			fmt.Sprintf(":%s QUIT :%s", c.prefix(), reason),
+			fmt.Sprintf(":%s QUIT :Quit: %s", c.prefix(), reason),
 			clientID(c.nickname()),
 			false,
 		)
