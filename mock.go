fccd09b750dd05fde34d526351167e5ec7d776ac
Salim Nassim <salim.nassim@gmail.com>
2024-02-24 14:52:46 -0500

    add addresses for mock conn

--- mock.go
+++ mock.go
@@ -15,5 +15,15 @@ func (c *connMock) SetReadDeadline(t time.Time) error  { c.deadline = t; return 
 func (c *connMock) SetWriteDeadline(t time.Time) error { c.deadline = t; return nil }
 func (c *connMock) Write(b []byte) (int, error)        { return 0, nil }
 func (c *connMock) Close() error                       { return nil }
-func (C *connMock) LocalAddr() net.Addr                { return &net.IPAddr{} }
-func (C *connMock) RemoteAddr() net.Addr               { return &net.IPAddr{} }
+func (C *connMock) LocalAddr() net.Addr {
+	return &net.TCPAddr{
+		IP:   net.IPv4(0x7F, 0x00, 0x00, 0x01),
+		Port: 5000,
+	}
+}
+func (C *connMock) RemoteAddr() net.Addr {
+	return &net.TCPAddr{
+		IP:   net.IPv4(0x7F, 0x00, 0x00, 0x01),
+		Port: 5001,
+	}
+}
