bf97405dedc1e5393d77867830e4bd24a8a14588
Salim Nassim <salim.nassim@gmail.com>
2024-02-25 20:52:01 -0500

    remove ELIST

--- handle_command_version.go
+++ handle_command_version.go
@@ -14,7 +14,7 @@ func handleVersion(s *server, c clienter, m message) {
 		// todo: get tokens from config state and actually enforce them
 		c.sendRPL(s.name, rplISupport{
 			client: c.nickname(),
-			tokens: "AWAYLEN=128 CASEMAPPING=ascii CHANLIMIT=#&:64 CHANNELLEN=50 CHANTYPES=#& ELIST=CUM HOSTLEN=128 KICKLEN=128 MODES=24 NICKLEN=31 PREFIX=(qaohv)~&@%+ TOPICLEN=307 USERLEN=18",
+			tokens: "AWAYLEN=128 CASEMAPPING=ascii CHANLIMIT=#&:64 CHANNELLEN=50 CHANTYPES=#& HOSTLEN=128 KICKLEN=128 MODES=24 NICKLEN=31 PREFIX=(qaohv)~&@%+ TOPICLEN=307 USERLEN=18",
 		})
 		return
 	}
