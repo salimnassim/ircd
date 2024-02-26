d5405e23b76b48649118be0803493aa0ce1bdddb
Salim Nassim <salim.nassim@gmail.com>
2024-02-25 23:07:11 -0500

    use ref to clienter instead of id add mode commands for channel membership

--- store_channel_client.go
+++ store_channel_client.go
@@ -1,6 +1,9 @@
 package ircd
 
 import (
+	"cmp"
+	"fmt"
+	"slices"
 	"sync"
 )
 
@@ -10,22 +13,27 @@ type channelClientStorer interface {
 	// Add client to channel.
 	add(c clienter)
 	// Delete client from channel.
-	delete(ID clientID)
+	delete(c clienter)
 	// Get all channel clients.
 	all() []clienter
 	// Is client member of the channel?
-	isMember(ID clientID) bool
+	isMember(c clienter) bool
+
+	addMode(c clienter, m channelMembershipMode)
+	removeMode(c clienter, m channelMembershipMode)
+	hasMode(c clienter, m ...channelMembershipMode) bool
+	modestring(c clienter) string
 }
 
 type channelClientStore struct {
 	mu      *sync.RWMutex
-	clients map[clientID]clienter
+	clients map[clienter]channelMembershipMode
 }
 
 func newChannelClientStore() *channelClientStore {
 	return &channelClientStore{
 		mu:      &sync.RWMutex{},
-		clients: make(map[clientID]clienter),
+		clients: make(map[clienter]channelMembershipMode),
 	}
 }
 
@@ -39,13 +47,13 @@ func (s *channelClientStore) count() int {
 
 func (s *channelClientStore) add(c clienter) {
 	s.mu.Lock()
-	s.clients[c.id()] = c
+	s.clients[c] = 0
 	s.mu.Unlock()
 }
 
-func (s *channelClientStore) delete(ID clientID) {
+func (s *channelClientStore) delete(c clienter) {
 	s.mu.Lock()
-	delete(s.clients, ID)
+	delete(s.clients, c)
 	s.mu.Unlock()
 }
 
@@ -53,7 +61,7 @@ func (s *channelClientStore) all() []clienter {
 	clients := []clienter{}
 
 	s.mu.RLock()
-	for _, c := range s.clients {
+	for c, _ := range s.clients {
 		clients = append(clients, c)
 	}
 	s.mu.RUnlock()
@@ -61,9 +69,79 @@ func (s *channelClientStore) all() []clienter {
 	return clients
 }
 
-func (s *channelClientStore) isMember(ID clientID) bool {
+func (s *channelClientStore) isMember(c clienter) bool {
 	s.mu.RLock()
-	_, ok := s.clients[ID]
+	_, ok := s.clients[c]
 	s.mu.RUnlock()
 	return ok
+}
+
+func (s *channelClientStore) addMode(c clienter, mode channelMembershipMode) {
+	if s.hasMode(c, mode) {
+		return
+	}
+
+	s.mu.RLock()
+	current, ok := s.clients[c]
+	s.mu.RUnlock()
+	if !ok {
+		return
+	}
+
+	current |= mode
+
+	s.mu.Lock()
+	s.clients[c] = current
+	s.mu.Unlock()
+}
+
+func (s *channelClientStore) removeMode(c clienter, mode channelMembershipMode) {
+	if !s.hasMode(c, mode) {
+		return
+	}
+
+	s.mu.RLock()
+	current, ok := s.clients[c]
+	s.mu.RUnlock()
+	if !ok {
+		return
+	}
+
+	s.mu.Lock()
+	current |= mode
+	s.clients[c] = current
+	s.mu.Unlock()
+}
+
+func (s *channelClientStore) hasMode(c clienter, modes ...channelMembershipMode) bool {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	current, ok := s.clients[c]
+	if !ok {
+		return false
+	}
+
+	has := false
+	for _, mode := range modes {
+		if current&mode != 0 {
+			has = true
+			break
+		}
+	}
+
+	return has
+}
+
+func (s *channelClientStore) modestring(c clienter) string {
+	modes := []rune{}
+	for m, r := range channelMembershipModeMap {
+		if s.hasMode(c, r) {
+			modes = append(modes, m)
+		}
+	}
+	slices.SortFunc[[]rune, rune](modes, func(a rune, b rune) int {
+		return cmp.Compare(a, b)
+	})
+	return fmt.Sprintf("+%s", string(modes))
 }
